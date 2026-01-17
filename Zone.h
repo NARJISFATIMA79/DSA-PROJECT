97c621395d29432ea9740af62be29b0a69e79770
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-17 16:45:48 +0500

    Class Zone data members

---
+++ Zone.h
@@ -0,0 +1,15 @@
+// Zone.h 
+#ifndef ZONE_H
+#define ZONE_H
+
+#include "ParkingArea.h"
+
+class Zone {
+private:
+    int zoneID;
+    ParkingArea* areas;
+    int areaCount;
+    int capacity;
+
+public:
+};
