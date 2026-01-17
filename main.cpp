f6e245f83718d705e7d558921809dade3ce3d003
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-17 16:43:11 +0500

    Parking Menu

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "ParkingSystem.h"
+
+using namespace std;
+
+void displayMenu() {
+    cout << "\n============================================" << endl;
+    cout << "   SMART PARKING MANAGEMENT SYSTEM v2.0    " << endl;
+    cout << "============================================" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| SETUP & CONFIGURATION                    |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| 1.  Setup Zone                           |" << endl;
+    cout << "| 2.  Expand City Zones                    |" << endl;
+    cout << "| 3.  View Active Zones                    |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| PARKING OPERATIONS                       |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| 4.  Create Parking Request               |" << endl;
+    cout << "| 5.  Allocate Parking                     |" << endl;
+    cout << "| 6.  Occupy Parking                       |" << endl;
+    cout << "| 7.  Release Parking                      |" << endl;
+    cout << "| 8.  Cancel Request                       |" << endl;
+    cout << "| 9.  Rollback Last K Allocations          |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| ANALYTICS & REPORTS                      |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| 10. System Dashboard                     |" << endl;
+    cout << "| 11. View Zone Details                    |" << endl;
+    cout << "| 12. View All Requests Status             |" << endl;
+    cout << "| 13. Analytics Summary                    |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "| 0.  Exit System                          |" << endl;
+    cout << "+------------------------------------------+" << endl;
+    cout << "Enter choice: ";
+}
