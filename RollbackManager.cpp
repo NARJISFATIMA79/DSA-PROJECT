e361bdc315b6bf848c5bf53f832998c29eee4dad
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-20 22:19:50 +0500

    Implementing Linkedlist

--- RollbackManager.cpp
+++ RollbackManager.cpp
@@ -5,3 +5,13 @@ RollbackManager::RollbackManager() : top(nullptr), size(0) {}
 RollbackManager::~RollbackManager() {
     clear();
 }
+
+void RollbackManager::pushOperation(int reqIndex, int slotID, int zoneID) {
+    RollbackOperation* newOp = new RollbackOperation;
+    newOp->requestIndex = reqIndex;
+    newOp->slotID = slotID;
+    newOp->zoneID = zoneID;
+    newOp->next = top;
+    top = newOp;
+    size++;
+}
