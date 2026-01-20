38749240ad95a5f0aad6273a6d49d83c15e18a8b
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-20 22:16:36 +0500

    Getting Id function in Vehicle.cpp

---
+++ Vehicle.cpp
@@ -0,0 +1,12 @@
+// ===== Vehicle.cpp =====
+#include "Vehicle.h"
+
+Vehicle::Vehicle() : vehicleID(0), preferredZone(0) {}
+
+Vehicle::Vehicle(int vID, int prefZone) : vehicleID(vID), preferredZone(prefZone) {}
+
+int Vehicle::getVehicleID() const {
+    return vehicleID;
+}
+
+
