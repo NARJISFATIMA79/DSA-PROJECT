be3f7a20a5f4b3f96735277bae4ea049c55e759b
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-23 21:56:22 +0500

    Getting Vehicle Id and Requested Zone

--- ParkingRequest.cpp
+++ ParkingRequest.cpp
@@ -3,3 +3,18 @@
 ParkingRequest::ParkingRequest() 
     : vehicleID(0), requestedZone(0), requestTime(0), state(REQUESTED), 
       allocatedSlotID(-1), allocatedZoneID(-1), releaseTime(0), crossZoneAllocation(false) {}
+ParkingRequest::ParkingRequest(int vID, int reqZone, int reqTime)
+    : vehicleID(vID), requestedZone(reqZone), requestTime(reqTime), state(REQUESTED),
+      allocatedSlotID(-1), allocatedZoneID(-1), releaseTime(0), crossZoneAllocation(false) {}
+
+int ParkingRequest::getVehicleID() const {
+    return vehicleID;
+}
+
+int ParkingRequest::getRequestedZone() const {
+    return requestedZone;
+}
+
+int ParkingRequest::getRequestTime() const {
+    return requestTime;
+}
