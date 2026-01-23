2cba2c83105e13a6977a4af70356675e6ed8bfff
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-23 22:01:52 +0500

    Function Declaration

--- ParkingArea.h
+++ ParkingArea.h
@@ -13,4 +13,18 @@ private:
 
 public:
     ParkingArea();
+
+     ParkingArea(int aID, int zID, int cap);
+    ~ParkingArea();
+    
+    ParkingArea(const ParkingArea& other);
+    ParkingArea& operator=(const ParkingArea& other);
+    
+    void addSlot(int slotID);
+    ParkingSlot* findAvailableSlot();
+    ParkingSlot* getSlotByID(int slotID);
+    int getSlotCount() const;
+    int getAvailableCount() const;
 };
+
+#endif
