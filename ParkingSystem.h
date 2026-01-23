5ff9367b7c417ea78b5ff1262e3e0c8cadf8e158
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-23 21:39:28 +0500

    Core functionality declaration

--- ParkingSystem.h
+++ ParkingSystem.h
@@ -24,3 +24,14 @@ private:
     void updateAllocationEngine();
     bool isValidZoneID(int zoneID) const;
     void resizeZoneArray(int newCapacity);
+
+    public:
+    ParkingSystem(int cityZones);
+    ~ParkingSystem();
+    
+    // Core functionality
+    bool setupZone(int zoneID, int numAreas, int slotsPerArea, char* errorMsg = nullptr);
+    bool expandCityZones(int additionalZones, char* errorMsg = nullptr);
+    int createRequest(int vehicleID, int requestedZone, int requestTime, char* errorMsg = nullptr);
+    bool allocateParking(int requestIndex, char* errorMsg = nullptr);
+    bool occupyParking(int requestIndex, char* errorMsg = nullptr);
