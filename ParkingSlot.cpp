ac5d3812a641e068e84f330add120aa0590ee28b
NARJISFATIMA79 <fatimanarjis681@gmail.com>
2026-01-23 21:52:54 +0500

    Setter and getter for availability

--- ParkingSlot.cpp
+++ ParkingSlot.cpp
@@ -7,3 +7,15 @@ ParkingSlot::ParkingSlot(int sID, int zID) : slotID(sID), zoneID(zID), isAvailab
 int ParkingSlot::getSlotID() const {
     return slotID;
 }
+
+int ParkingSlot::getZoneID() const {
+    return zoneID;
+}
+
+bool ParkingSlot::getAvailability() const {
+    return isAvailable;
+}
+
+void ParkingSlot::setAvailability(bool status) {
+    isAvailable = status;
+}
